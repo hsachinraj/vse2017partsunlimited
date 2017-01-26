311530c1bdb29ba28524a54a5ecea1886d0cca76
Sachin Raj <sachin77@outlook.com>
2017-01-26 19:01:56 +0000

    adding Partsunlimited app

---
+++ PartsUnlimited/src/PartsUnlimitedWebsite/ProductSearch/StringContainsProductSearch.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using PartsUnlimited.Models;
+using System;
+
+namespace PartsUnlimited.ProductSearch
+{
+    public class StringContainsProductSearch : IProductSearch
+    {
+        private readonly IPartsUnlimitedContext _context;
+
+        public StringContainsProductSearch(IPartsUnlimitedContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Product>> Search(string query)
+        {
+            var lowercase_query = query.ToLower();
+
+            var q = _context.Products
+                .Where(p => p.Title.ToLower().Contains(lowercase_query));
+
+            if (q == null || q.Count() == 0)
+            {
+                try
+                {
+                    throw new System.Exception("Missing or Can't find product: " + lowercase_query);
+                }
+                catch(Exception)
+                {
+                    //log this for product identification
+                }
+
+            }
+            
+            return await q.ToListAsync();
+        }
+    }
+}
