813f48b81daa97b72433bc4899c4b2dff229bc4e
Sachin <sraj@microsoft.com>
2017-02-05 21:26:40 -0800

    Adding PULe2e

---
+++ PULE2E/PartsUnlimited-aspnet45/PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite.UnitTests/Controllers/OrdersControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PartsUnlimited.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartsUnlimited.Controllers.Tests
+{
+    [TestClass()]
+    public class OrdersControllerTests
+    {
+        [TestMethod()]
+        public void DetailsTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
